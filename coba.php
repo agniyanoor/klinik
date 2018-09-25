c3860e385c3837194e47e56bb5347c9327bdbe92
Zuliana Sandra Prastiwi <43576188+zulianasandra@users.noreply.github.com>
2018-09-25 22:28:29 +0700

    commit 1 zuliana

---
+++ coba.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Anda masuk sebagai, Admin!</h1>
+<form action="/action_page.php" >
+<p>Masukkan ID Pasien: </p>
+<input type="text" name="idpasien" placeholder="ID Pasien">
+<br>
+<button type="submit">Submit</button>
+<hr>
+<h2>INVOICE</h2>
+<input type="text" name="namapasien" placeholder="Nama Pasien">
+<br>
+<input type="text" name="totaltagihan" placeholder="Total Tagihan">
+<br>
+<input type="text" name="detiltagihan" placeholder="Detil Tagihan">
+<br>
+<input type="text" name="statuspembayaran" placeholder="Status Pembayaran">
+<br>
+<button type="submit">Cetak Invoice</button>
+</form>
+</body>
+</html>
